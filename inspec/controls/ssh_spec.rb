60d89901a59ebe9987c417b4ae0fa1a517ee8e71
Tobias Kongsvik <tokongs98@gmail.com>
2021-03-20 17:03:47 +0100

    update to way we get user ca key

--- inspec/controls/ssh_spec.rb
+++ inspec/controls/ssh_spec.rb
@@ -2,3 +2,8 @@ describe service('sshd') do
     it { should be_enabled }
     it { should be_running }
 end
+
+describe file('/etc/ssh/trusted-user-ca-keys.pem') do
+    it { should exist }
+    its('content') { should_not be_empty }
+end
